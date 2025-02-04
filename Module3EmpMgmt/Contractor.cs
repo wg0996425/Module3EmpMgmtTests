ab907ad6c9a8c8e2062f06a0684bc9d19f91cf37
wg0996425 <wg0996425@otc.edu>
2025-02-04 10:04:22 -0600

    Further set up for testing

--- Module3EmpMgmt/Contractor.cs
+++ Module3EmpMgmt/Contractor.cs
@@ -24,7 +24,8 @@ namespace Module3EmpMgmt
         public override void CalculatePay()
         {
             // Salary is based upon the total number of days from the current days until the expiry date multiplied by hourly pay, multiplied by hours per day worked
-            var BaseSalary = (ContractExpiryDate - DateTime.Today).TotalDays * 30 * 10;
+            var startdate = new DateTime(2025, 01, 01);
+            var BaseSalary = (ContractExpiryDate - startdate).TotalDays * 30 * 10;
             Console.WriteLine($"Contractor Pay: {BaseSalary}");
         }
 
